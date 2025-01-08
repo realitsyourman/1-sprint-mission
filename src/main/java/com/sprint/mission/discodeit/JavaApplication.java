98c2fd586cd2728222b0d4f0401a88c427c4a146
realitsyourman <realitsyourman@gmail.com>
2025-01-08 17:56:31 +0900

    update JCFChannelService update readChannelInfo method

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -11,12 +11,17 @@ import java.util.List;
 
 public class JavaApplication {
     public static void main(java.lang.String[] args) {
-        User user1 = new User("사용자1", "dsadsd", "1234");
-        Channel channel1 = new Channel("채널1", user1, Arrays.asList(user1));
+        /**
+         * ChannelService 인터페이스를 구현한 JCFChannelService 클래스를 사용
+         */
+        User user1 = new User("사용자1", "o134maka@gmail.com", "1234");
+        User user2 = new User("사용자2", "dsadsd", "thisispassword");
+        Channel channel1 = new Channel("채널1", user1, Arrays.asList(user1, user2));
         List<Channel> channelList = new ArrayList<>();
 
         ChannelService channelService = new JCFChannelService(channelList);
         channelService.createChannel(channel1);
         channelService.readChannelInfo("채널1");
+        channelService.readAllChannels();
     }
 }
--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -4,6 +4,8 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.ChannelService;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class JCFChannelService implements ChannelService {
@@ -20,12 +22,18 @@ public class JCFChannelService implements ChannelService {
 
     @Override
     public void readChannelInfo(String channelName) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date data = new Date();
+
         channelList.stream()
                 .filter(readChannel -> readChannel.getChannelName().equals(channelName))
                 .forEach(readChannel -> {
+                    data.setTime(readChannel.getCreatedAt());
+                    String createdAt = simpleDateFormat.format(data); /* 유닉스 타임스탬프 변경 */
+
                     System.out.println("채널 이름: " + readChannel.getChannelName());
                     System.out.println("채널 소유자: " + readChannel.getChannelOwnerUser().getUserName());
-                    System.out.println("채널 생성시간: " + readChannel.getCreatedAt());
+                    System.out.println("채널 생성시간: " + createdAt);
                 });
     }
 
