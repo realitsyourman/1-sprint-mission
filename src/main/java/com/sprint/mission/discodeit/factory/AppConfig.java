5522d9b4311f67115689f720ceb77bbb6c7c8423
realitsyourman <realitsyourman@gmail.com>
2025-01-21 16:21:03 +0900

    feat: AppConfig.java

---
+++ src/main/java/com/sprint/mission/discodeit/factory/AppConfig.java
@@ -0,0 +1,29 @@
+package com.sprint.mission.discodeit.factory;
+
+import com.sprint.mission.discodeit.repository.jcf.JCFChannelRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
+
+public class AppConfig {
+
+    public ChannelService basicChannelService() {
+        return new BasicChannelService(new JCFChannelRepository());
+        //return new BasicChannelService(new FileChannelRepository());
+
+    }
+
+    public UserService basicUserService() {
+        return new BasicUserService(new JCFUserRepository(),
+                new JCFChannelRepository());
+    }
+
+    public MessageService basicMessageService() {
+        return new BasicMessageService(new JCFMessageRepository());
+    }
+}
