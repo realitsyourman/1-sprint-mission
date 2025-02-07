bf2459009bdbec48915fb802442c53ad102acc91
realitsyourman <realitsyourman@gmail.com>
2025-02-07 15:01:02 +0900

    chore: add parameters

--- src/main/java/com/sprint/mission/discodeit/factory/BaseEntityFactory.java
+++ src/main/java/com/sprint/mission/discodeit/factory/BaseEntityFactory.java
@@ -1,8 +1,9 @@
 package com.sprint.mission.discodeit.factory;
 
-import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.channel.Channel;
+import com.sprint.mission.discodeit.entity.message.Message;
+import com.sprint.mission.discodeit.entity.user.User;
+import com.sprint.mission.discodeit.entity.user.UserRole;
 
 import java.util.Map;
 import java.util.UUID;
@@ -20,17 +21,32 @@ public class BaseEntityFactory implements EntityFactory {
     }
 
     @Override
+    public User createUser(String userName, String userEmail, String userPassword, UserRole userRole) {
+        return new User(userName, userEmail, userPassword, userRole);
+    }
+
+    @Override
+    public Channel createChannel(UUID channelId) {
+        return new Channel(channelId);
+    }
+
+    @Override
     public User createUser(String userName, String userEmail, String userPassword) {
         return new User(userName, userEmail, userPassword);
     }
 
     @Override
-    public Channel createChannel(String channelName, User owner, Map<UUID, User> userList) {
-        return new Channel(channelName, owner, userList);
+    public Channel createChannel(String channelName, User owner, String channelType, Map<UUID, User> userList) {
+        return new Channel(channelName, owner, channelType, userList);
     }
 
     @Override
     public Message createMessage(String title, String content, User sender, User receiver) {
         return new Message(title, content, sender, receiver);
+    }
+
+    @Override
+    public Message createMessage(UUID messageId, String title, String content, User sender, User receiver) {
+        return new Message(messageId, title, content, sender, receiver);
     }
 }
--- src/main/java/com/sprint/mission/discodeit/factory/EntityFactory.java
+++ src/main/java/com/sprint/mission/discodeit/factory/EntityFactory.java
@@ -1,8 +1,9 @@
 package com.sprint.mission.discodeit.factory;
 
-import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.channel.Channel;
+import com.sprint.mission.discodeit.entity.message.Message;
+import com.sprint.mission.discodeit.entity.user.User;
+import com.sprint.mission.discodeit.entity.user.UserRole;
 
 import java.util.Map;
 import java.util.UUID;
@@ -10,7 +11,12 @@ import java.util.UUID;
 public interface EntityFactory {
     User createUser(String userName, String userEmail, String userPassword);
 
-    Channel createChannel(String channelName, User owner, Map<UUID, User> userList);
+    User createUser(String userName, String userEmail, String userPassword, UserRole userRole);
+
+    Channel createChannel(UUID channelId);
+
+    Channel createChannel(String channelName, User owner, String channelType, Map<UUID, User> userList);
 
     Message createMessage(String title, String content, User sender, User receiver);
+    Message createMessage(UUID messageId, String title, String content, User sender, User receiver);
 }
