01f5e8c656ece63e3a1209b2684edd0ff4df91a3
realitsyourman <realitsyourman@gmail.com>
2025-02-07 15:07:53 +0900

    chore: remove Bean Configuration Class

--- src/main/java/com/sprint/mission/discodeit/repository/RepositoryConfig.java
+++ src/main/java/com/sprint/mission/discodeit/repository/RepositoryConfig.java
@@ -1,25 +1,25 @@
-package com.sprint.mission.discodeit.repository;
-
-import com.sprint.mission.discodeit.repository.file.json.JsonChannelRepository;
-import com.sprint.mission.discodeit.repository.file.json.JsonMessageRepository;
-import com.sprint.mission.discodeit.repository.file.json.JsonUserRepository;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RepositoryConfig {
-    @Bean
-    UserRepository userRepository() {
-        return new JsonUserRepository();
-    }
-
-    @Bean
-    MessageRepository messageRepository() {
-        return new JsonMessageRepository();
-    }
-
-    @Bean
-    ChannelRepository channelRepository() {
-        return new JsonChannelRepository();
-    }
-}
+//package com.sprint.mission.discodeit.repository;
+//
+//import com.sprint.mission.discodeit.repository.file.json.JsonChannelRepository;
+//import com.sprint.mission.discodeit.repository.file.json.JsonMessageRepository;
+//import com.sprint.mission.discodeit.repository.file.json.JsonUserRepository;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RepositoryConfig {
+//    @Bean
+//    UserRepository userRepository() {
+//        return new JsonUserRepository();
+//    }
+//
+//    @Bean
+//    MessageRepository messageRepository() {
+//        return new JsonMessageRepository();
+//    }
+//
+//    @Bean
+//    ChannelRepository channelRepository() {
+//        return new JsonChannelRepository();
+//    }
+//}
--- src/main/java/com/sprint/mission/discodeit/service/ServiceConfig.java
+++ src/main/java/com/sprint/mission/discodeit/service/ServiceConfig.java
@@ -1,28 +1,51 @@
-package com.sprint.mission.discodeit.service;
-
-import com.sprint.mission.discodeit.repository.ChannelRepository;
-import com.sprint.mission.discodeit.repository.MessageRepository;
-import com.sprint.mission.discodeit.repository.UserRepository;
-import com.sprint.mission.discodeit.service.basic.BasicChannelService;
-import com.sprint.mission.discodeit.service.basic.BasicMessageService;
-import com.sprint.mission.discodeit.service.basic.BasicUserService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ServiceConfig {
-    @Bean
-    UserService userService(UserRepository userRepository) {
-        return new BasicUserService(userRepository);
-    }
-
-    @Bean
-    ChannelService channelService(ChannelRepository channelRepository, MessageService messageService) {
-        return new BasicChannelService(channelRepository, messageService);
-    }
-
-    @Bean
-    MessageService messageService(MessageRepository messageRepository) {
-        return new BasicMessageService(messageRepository);
-    }
-}
+//package com.sprint.mission.discodeit.service;
+//
+//import com.sprint.mission.discodeit.repository.*;
+//import com.sprint.mission.discodeit.repository.file.json.JsonBinaryContentRepository;
+//import com.sprint.mission.discodeit.repository.file.json.JsonUserStatusRepository;
+//import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+//import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+//import com.sprint.mission.discodeit.service.basic.BasicUserService;
+//import com.sprint.mission.discodeit.service.status.UserStateService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ServiceConfig {
+//    @Bean
+//    UserService userService(UserRepository userRepository,
+//                            UserStateService userStatusService,
+//                            BinaryContentService binaryContentService) {
+//
+//        return new BasicUserService(userRepository, userStatusService, binaryContentService);
+//    }
+//
+//    @Bean
+//    ChannelService channelService(ChannelRepository channelRepository, MessageService messageService, ReadStatusRepository readStatusRepository) {
+//        return new BasicChannelService(channelRepository, messageService, readStatusRepository);
+//    }
+//
+//    @Bean
+//    MessageService messageService(MessageRepository messageRepository,
+//                                  BinaryContentRepository binaryContentRepository,
+//                                  ChannelRepository channelRepository ) {
+//
+//        return new BasicMessageService(messageRepository, binaryContentRepository, channelRepository);
+//    }
+//
+//    @Bean
+//    UserStateService userStateService(UserRepository userRepository, UserStatusRepository userStatusRepository) {
+//        return new UserStateService(userRepository, userStatusRepository);
+//    }
+//
+//    @Bean
+//    BinaryContentRepository binaryContentRepository() {
+//        return new JsonBinaryContentRepository();
+//    }
+//
+//    @Bean
+//    UserStatusRepository userStatusRepository() {
+//        return new JsonUserStatusRepository();
+//    }
+//
+//}
