09bac058d76b7e5d06ab2dd1ae6a210228b328e0
realitsyourman <realitsyourman@gmail.com>
2025-02-07 14:58:05 +0900

    feat: add Binary Content Exception

---
+++ src/main/java/com/sprint/mission/discodeit/exception/BinaryContentException.java
@@ -0,0 +1,11 @@
+package com.sprint.mission.discodeit.exception;
+
+public class BinaryContentException extends RuntimeException {
+    public BinaryContentException() {
+        super();
+    }
+
+    public BinaryContentException(String message) {
+        super(message);
+    }
+}
