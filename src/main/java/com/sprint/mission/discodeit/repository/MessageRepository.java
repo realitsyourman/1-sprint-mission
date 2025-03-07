cef1295927b3600e3fbe53551c24682735681c44
realitsyourman <realitsyourman@gmail.com>
2025-03-07 17:16:50 +0900

    refactor: cursor based paging on Message Repository

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.repository;
 import com.sprint.mission.discodeit.entity.channel.Channel;
 import com.sprint.mission.discodeit.entity.message.Message;
 import com.sprint.mission.discodeit.entity.user.User;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
@@ -16,7 +17,9 @@ public interface MessageRepository extends JpaRepository<Message, UUID> {
   @Query("select m.author from Message m join m.channel where m.channel = :channel")
   List<User> findUserByChannel(@Param("channel") Channel channel);
 
-  Page<Message> findById(UUID id, Pageable pageable);
-
   Page<Message> findAllByChannelId(UUID channelId, Pageable pageable);
+
+  @Query("select m from Message m where m.channel.id = :channelId and m.createdAt > :cursor order by m.createdAt desc")
+  Page<Message> findAllByChannelIdWithCursor(@Param("channelId") UUID ChannelId,
+      @Param("cursor") Instant cursor, Pageable pageable);
 }
