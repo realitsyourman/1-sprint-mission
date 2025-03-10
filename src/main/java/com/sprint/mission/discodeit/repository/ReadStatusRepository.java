7dd0b38060c4299b2772600fae6df050bb121c01
realitsyourman <realitsyourman@gmail.com>
2025-03-10 15:36:21 +0900

    feat: apply fetch join on read status repository

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -4,19 +4,25 @@ import com.sprint.mission.discodeit.entity.channel.Channel;
 import com.sprint.mission.discodeit.entity.status.read.ReadStatus;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
 
+  @EntityGraph(attributePaths = {"user", "channel"})
   ReadStatus findFirstByChannelOrderByLastReadAt(Channel channel);
 
-  @Query("select r from ReadStatus r join fetch r.channel c where r.user.id = :userId")
+  @EntityGraph(attributePaths = {"user.profile", "user.status", "channel"})
+  @Query("select r from ReadStatus r where r.user.id = :userId")
   List<ReadStatus> findAllChannelsInUser(@Param("userId") UUID userId);
 
+  @EntityGraph(attributePaths = {"user.profile", "user.status", "channel"})
   List<ReadStatus> findAllByChannel(Channel channel);
 
+
+  // test
   List<ReadStatus> findByChannel_Id(UUID channelId);
 
   List<ReadStatus> findAllByUser_Id(UUID userId);
--- src/main/java/com/sprint/mission/discodeit/service/status/ReadStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/status/ReadStatusService.java
@@ -15,8 +15,10 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ReadStatusService {
 
