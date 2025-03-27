7d86b0e592cd75802d12f9ff3eb4e6b7b7dc4466
WONGIL <realitsyourman@gmail.com>
2025-03-27 15:49:57 +0900

    feat: add BinaryContent Exception

--- src/main/java/com/sprint/mission/discodeit/storage/LocalBinaryContentStorage.java
+++ src/main/java/com/sprint/mission/discodeit/storage/LocalBinaryContentStorage.java
@@ -1,13 +1,19 @@
 package com.sprint.mission.discodeit.storage;
 
 import com.sprint.mission.discodeit.dto.response.BinaryContentDto;
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import com.sprint.mission.discodeit.exception.binary.FailReadBinaryContent;
+import com.sprint.mission.discodeit.exception.binary.NotSavedBinaryContentException;
 import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Map;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.InputStreamResource;
@@ -16,6 +22,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @ConditionalOnProperty(name = "discodeit.storage.type", havingValue = "local")
 public class LocalBinaryContentStorage implements BinaryContentStorage {
@@ -45,8 +52,12 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
     Path filePath = resolvePath(fileId);
     try {
       Files.write(filePath, bytes);
+      log.info("파일 저장: {}", fileId);
     } catch (IOException e) {
-      throw new RuntimeException("파일 저장 실패: ", e);
+      log.error("파일 저장 실패: {}", fileId);
+      throw new NotSavedBinaryContentException(Instant.now(), ErrorCode.NOT_SAVED_FILE,
+          Map.of("fileId", ErrorCode.NOT_SAVED_FILE.getMessage())
+      );
     }
 
     return fileId;
@@ -59,7 +70,9 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
     try {
       return Files.newInputStream(filePath);
     } catch (IOException e) {
-      throw new RuntimeException("파일 읽기 실패: ", e);
+      log.error("파일 읽기 실패: {}", fileId);
+      throw new FailReadBinaryContent(Instant.now(), ErrorCode.FAIL_READ_FILE,
+          Map.of("fileId", ErrorCode.FAIL_READ_FILE.getMessage()));
     }
 
   }
@@ -69,6 +82,7 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
     InputStream findFileStream = get(file.id());
 
     InputStreamResource fileResource = new InputStreamResource(findFileStream);
+    log.info("파일 다운로드: {}", file.fileName());
 
     return ResponseEntity.ok()
         .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.fileName() + "\"")
