5a38171f988c13f593fcae383cec04437cb5daff
WONGIL <realitsyourman@gmail.com>
2025-03-28 11:19:06 +0900

    chore: remove other exception

--- src/main/java/com/sprint/mission/discodeit/exception/DiscodeitException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/DiscodeitException.java
@@ -2,26 +2,12 @@ package com.sprint.mission.discodeit.exception;
 
 import java.time.Instant;
 import java.util.Map;
+import lombok.Getter;
 
+@Getter
 public class DiscodeitException extends RuntimeException {
 
   public DiscodeitException(Instant timestamp, ErrorCode errorCode, Map<String, Object> details) {
-    this.timestamp = timestamp;
-    this.errorCode = errorCode;
-    this.details = details;
-  }
-
-  public DiscodeitException(String message, Instant timestamp, ErrorCode errorCode,
-      Map<String, Object> details) {
-    super(message);
-    this.timestamp = timestamp;
-    this.errorCode = errorCode;
-    this.details = details;
-  }
-
-  public DiscodeitException(String message, Throwable cause, Instant timestamp, ErrorCode errorCode,
-      Map<String, Object> details) {
-    super(message, cause);
     this.timestamp = timestamp;
     this.errorCode = errorCode;
     this.details = details;
