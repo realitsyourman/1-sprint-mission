89de336786c5e428f632d979a863bbe1a8f8c517
WONGIL <realitsyourman@gmail.com>
2025-03-28 11:14:27 +0900

    feat: add BinaryContentExceptionAdvice

---
+++ src/main/java/com/sprint/mission/discodeit/exception/BinaryContentExceptionAdvice.java
@@ -0,0 +1,27 @@
+package com.sprint.mission.discodeit.exception;
+
+import com.sprint.mission.discodeit.exception.binary.BinaryContentNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class BinaryContentExceptionAdvice {
+
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(BinaryContentNotFoundException.class)
+  public ErrorResponse notFoundFile(BinaryContentNotFoundException e) {
+
+    return ErrorResponse.builder()
+        .timestamp(e.getTimestamp())
+        .code(e.getErrorCode().getCode())
+        .message(e.getMessage())
+        .details(e.getDetails())
+        .exceptionType(e.getClass().getSimpleName())
+        .status(HttpStatus.BAD_REQUEST.value())
+        .build();
+  }
+}
