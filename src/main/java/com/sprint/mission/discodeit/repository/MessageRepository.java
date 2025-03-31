9278e1dc2441b56f65a4dfd5c7e60e9467545125
WONGIL <realitsyourman@gmail.com>
2025-03-31 15:27:16 +0900

    chore: deprecate findAllByChannelIdWithCursor method in Message Repository

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -1,17 +1,13 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.message.Message;
-import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface MessageRepository extends JpaRepository<Message, UUID>, MessageQuerydslRepository {
 
@@ -22,17 +18,6 @@ public interface MessageRepository extends JpaRepository<Message, UUID>, Message
   @EntityGraph(attributePaths = {"channel", "author", "author.profile", "attachments",
       "author.status"})
   Optional<Message> findById(@NonNull UUID uuid);
-
-  @Query("select m from Message m"
-      + " join fetch m.channel c"
-      + " join fetch m.author a"
-      + " join fetch a.profile p"
-      + " join fetch a.status st"
-      + " left join fetch m.attachments at"
-      + " where m.channel.id = :channelId"
-      + " and m.createdAt > :cursor")
-  Slice<Message> findAllByChannelIdWithCursor(@Param("channelId") UUID ChannelId,
-      @Param("cursor") Instant cursor, Pageable slice);
 
   @EntityGraph(attributePaths = {"attachments"})
   @Override
