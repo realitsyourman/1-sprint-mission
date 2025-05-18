d22e83bc2eb7c60125171426f2769f6b763047ff
WONGIL <realitsyourman@gmail.com>
2025-05-18 20:24:47 +0900

    docs: add Spring security debug mode

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -3,8 +3,10 @@ package com.sprint.mission.discodeit;
 import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class DiscodeitApplication {
 
   public static void main(String[] args) throws IOException {
