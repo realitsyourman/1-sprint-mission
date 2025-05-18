e6b6e363c03165d19998a3c05e719a8ce32aa7fa
WONGIL <realitsyourman@gmail.com>
2025-05-18 20:25:13 +0900

    feat: publish csrf token

--- src/main/java/com/sprint/mission/discodeit/controller/UserAuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserAuthController.java
@@ -6,7 +6,9 @@ import com.sprint.mission.discodeit.service.auth.UserAuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,10 @@ public class UserAuthController {
   @PostMapping("/login")
   public ResponseLogin login(@RequestBody @Validated RequestLogin request) {
     return userAuthService.login(request);
+  }
+
+  @GetMapping("/csrf-token")
+  public CsrfToken publishCsrfToken(CsrfToken csrfToken) {
+    return csrfToken;
   }
 }
