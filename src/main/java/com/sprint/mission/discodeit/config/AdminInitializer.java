7cb871e4bd80b4ef7d28200a0ca5e7c663dae137
WONGIL <realitsyourman@gmail.com>
2025-05-19 18:29:08 +0900

    feat: add admin initializer

---
+++ src/main/java/com/sprint/mission/discodeit/config/AdminInitializer.java
@@ -0,0 +1,41 @@
+package com.sprint.mission.discodeit.config;
+
+import com.sprint.mission.discodeit.entity.role.Role;
+import com.sprint.mission.discodeit.entity.status.user.UserStatus;
+import com.sprint.mission.discodeit.entity.user.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer implements ApplicationRunner {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+
+    if (userRepository.existsByUsername("admin")) {
+      return;
+    }
+
+    User admin = User.builder()
+        .username("admin")
+        .role(Role.ROLE_ADMIN)
+        .email("admin@mail.com")
+        .profile(null)
+        .password(passwordEncoder.encode("qwerqwer"))
+        .build();
+
+    UserStatus userStatus = new UserStatus(admin, Instant.now());
+    admin.changeUserStatus(userStatus);
+    
+    userRepository.save(admin);
+  }
+}
